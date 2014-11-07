b1e45a133931616e0a80e12be27ac90b0cc97f3d
davebshow <davebshow@gmail.com>
2014-11-07 17:25:04 -0500

    a few little changes on alpha release materials

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from distutils.core import setup
 setup(
     name='ProjX',
     version='0.a1.0',
+    url='http://projx.readthedocs.org/en/latest/#',
     license='MIT',
     author='davebshow',
     author_email='davebshow@gmail.com',
+    description='Wraps NetworkX for queries and schema manipulations',
+    long_description=open('README.txt').read(),
     install_requires=[
         'networkx == 1.9',
         'pyparsing==2.0.2'
