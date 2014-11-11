2ad4348841f1e9015ec7ee67c6a4db802f1d2061
davebshow <davebshow@gmail.com>
2014-11-11 15:38:18 -0500

    added disappearing package name

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 
 setup(
     name='ProjX',
-    version='0.a1.1',
+    version='0.1.0',
     url='http://projx.readthedocs.org/en/latest/#',
     license='MIT',
     author='davebshow',
     author_email='davebshow@gmail.com',
     description='Wraps NetworkX for queries and schema manipulations',
     long_description=open('README.txt').read(),
+    packages=['projx'],
     install_requires=[
         'networkx == 1.9',
         'pyparsing==2.0.2'
