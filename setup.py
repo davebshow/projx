323072a951e484528bb78c43617e104ce706b90b
davebshow <davebshow@gmail.com>
2014-11-17 10:59:34 -0500

    updated setup and changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='ProjX',
-    version='0.1.0',
+    version='0.1.2',
     url='http://projx.readthedocs.org/en/latest/#',
     license='MIT',
     author='davebshow',
