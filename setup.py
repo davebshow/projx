dfe94c5fb6a663c2a5bd60a3561abd930477f180
davebshow <davebshow@gmail.com>
2015-01-07 18:59:11 -0500

    updating changes.txt and setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from distutils.core import setup
 
 setup(
     name='projx',
-    version='0.2.5',
+    version='0.2.6',
     url='http://projx.readthedocs.org/en/latest/#',
     license='MIT',
     author='davebshow',
     author_email='davebshow@gmail.com',
     description='Graph transformations in Python',
-    long_description=open('README.txt').read(),
+    long_description=open('README.txt').read() + "\n\n" + open('CHANGES.txt').read(),
     packages=['projx'],
     install_requires=[
-        'networkx == 1.9',
+        'networkx==1.9',
         'pyparsing==2.0.2'
     ]
 )
