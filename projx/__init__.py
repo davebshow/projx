eb8f0dfc5305dc100335fe9f2acbb2e9c69217cf
davebshow <davebshow@gmail.com>
2015-01-09 10:36:11 -0500

    fixed import statement

--- projx/__init__.py
+++ projx/__init__.py
@@ -1,7 +1,7 @@
 from .api import Projection, execute_etl
 from .grammar import parse_query
-from .projector import (reset_index, match, traverse, project, transfer,
-						combine, build_subgraph, NXProjector)
+from .nxprojx import (reset_index, match, traverse, project, transfer,
+                      combine, build_subgraph, NXProjector)
 from .utils import (test_graph, project_etl, transfer_etl, combine_etl,
-				    multi_transform_etl, draw_simple_graph, remove_edges,
-				    proj_density)
+                    multi_transform_etl, draw_simple_graph, remove_edges,
+                    proj_density)
