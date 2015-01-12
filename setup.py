c31518694be98e0a43d6c3206cd66f58d2d4ffac
davebshow <davebshow@gmail.com>
2015-01-12 10:48:31 -0500

    removed changes from setup, causing error on install

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup(
     name='projx',
-    version='0.2.8',
+    version='0.2.9',
     url='http://projx.readthedocs.org/en/latest/#',
     license='MIT',
     author='davebshow',
     author_email='davebshow@gmail.com',
     description='Graph transformations in Python',
-    long_description=open('README.txt').read() + "\n\n" + open('CHANGES.txt').read(),
+    long_description=open('README.txt').read(),
     packages=['projx'],
     install_requires=[
         'networkx==1.9',
